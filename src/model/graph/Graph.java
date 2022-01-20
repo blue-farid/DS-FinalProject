e8be7fa92eeb9f9353dc9879447df7b9aa99037b
f_masjedi <f_masjedi@PC-MASJEDI>
2022-01-20 11:53:46 +0330

    impl constrcutor of Graph class

--- src/model/graph/Graph.java
+++ src/model/graph/Graph.java
@@ -3,6 +3,18 @@ package model.graph;
 import java.util.LinkedList;
 
 public class Graph {
-    private int size;
-    private LinkedList<Edge> adjacencyList;
+    private final int size;
+    private final LinkedList<Edge> []adjacencyList;
+
+    public Graph(int size) {
+        this.size = size;
+        adjacencyList = new LinkedList[size];
+        initializeAdjacencyList();
+    }
+
+    private void initializeAdjacencyList() {
+        for (int i = 0; i < this.size; i++) {
+            adjacencyList[i] = new LinkedList<>();
+        }
+    }
 }
