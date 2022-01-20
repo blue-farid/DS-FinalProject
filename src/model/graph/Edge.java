30ddca205e65e95540dfaf7b4d15548f2a4026f0
f_masjedi <f_masjedi@PC-MASJEDI>
2022-01-20 11:35:04 +0330

    add Edge class

---
+++ src/model/graph/Edge.java
@@ -0,0 +1,23 @@
+package model.graph;
+
+public class Edge {
+    private final int source, destination, weight;
+
+    public Edge(int source, int destination, int weight) {
+        this.source = source;
+        this.destination = destination;
+        this.weight = weight;
+    }
+
+    public int getSource() {
+        return source;
+    }
+
+    public int getDestination() {
+        return destination;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+}
