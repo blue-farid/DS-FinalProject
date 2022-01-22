9dfcb9073e7849a4e29a433664b103c881c04e7f
blue_farid <farid_127@outlook.com>
2022-01-22 15:38:19 +0330

    Move All of the things to FindTheFairest... class

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import model.FindTheFairestCoffeeShopApplication;
 import model.graph.Graph;
 import model.graph.Node;
 
@@ -6,21 +7,6 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        Graph graph = getGraphFromStdin(scanner);
-        System.out.println(graph.toString());
-    }
-
-    private static Graph getGraphFromStdin(Scanner sc) {
-        int nodesCount = sc.nextInt();
-        int edgesCount = sc.nextInt();
-        Graph graph = new Graph(nodesCount, edgesCount);
-        sc.nextLine();
-        sc.nextLine(); //ignore the line of nodes input. (that is not needed).
-        for (int i = 0; i < edgesCount; i++) {
-            graph.addEdge(new Node(sc.nextInt()), new Node(sc.nextInt()), sc.nextInt());
-            sc.nextLine();
-        }
-        return graph;
+        new FindTheFairestCoffeeShopApplication().run();
     }
 }
