866449ab1ffbdbeff61184121172bbc1166aef2f
f_masjedi <f_masjedi@PC-MASJEDI>
2022-01-23 08:37:11 +0330

    add javadoc to Main

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,15 @@
 import model.FindTheFairestCoffeeShopApplication;
 
+/**
+ * The Main class
+ */
 public class Main {
 
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
     public static void main(String[] args) {
         new FindTheFairestCoffeeShopApplication().run();
     }
