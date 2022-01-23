eae795483394eeddfaa6de13b588ae1e9e8a76c8
f_masjedi <f_masjedi@PC-MASJEDI>
2022-01-23 08:55:44 +0330

    add javadoc to Edge class

--- src/model/graph/Edge.java
+++ src/model/graph/Edge.java
@@ -1,15 +1,30 @@
 package model.graph;
 
+/**
+ * The Edge class
+ */
 public class Edge {
     private final Node first, second;
     private final int weight;
 
+    /**
+     * Instantiates a new Edge.
+     *
+     * @param first  the first
+     * @param second the second
+     * @param weight the weight
+     */
     public Edge(Node first, Node second, int weight) {
         this.first = first;
         this.second = second;
         this.weight = weight;
     }
-    
+
+    /**
+     * Gets weight.
+     *
+     * @return the weight
+     */
     public int getWeight() {
         return weight;
     }
@@ -19,6 +34,12 @@ public class Edge {
         return "(" + first + ", " + second + ", " + weight + ")";
     }
 
+    /**
+     * To string string.
+     *
+     * @param node the node
+     * @return the string
+     */
     public String toString(Node node) {
         if (node.equals(first)) {
             return second + "";
@@ -29,6 +50,12 @@ public class Edge {
         }
     }
 
+    /**
+     * Gets edge of a node
+     *
+     * @param node the node
+     * @return the edge of the node (input)
+     */
     public Node getEdgeOf(Node node) {
         if (node.equals(first)) {
             return second;
