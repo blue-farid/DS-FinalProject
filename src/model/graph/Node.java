e69e85e940435d9840d770488ac6c6cfd9c9eb67
f_masjedi <f_masjedi@PC-MASJEDI>
2022-01-23 08:59:19 +0330

    add javadoc to Node class

--- src/model/graph/Node.java
+++ src/model/graph/Node.java
@@ -4,24 +4,47 @@ import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Objects;
 
+/**
+ * The Node class
+ */
 public class Node implements Comparable<Node>, Comparator<Node> {
     private final int data;
     private int cost;
     private HashMap<Node, Integer> dijkstraResultsMap;
     private float fairScore;
 
+    /**
+     * Sets cost.
+     *
+     * @param cost the cost
+     */
     public void setCost(int cost) {
         this.cost = cost;
     }
 
+    /**
+     * Gets cost.
+     *
+     * @return the cost
+     */
     public int getCost() {
         return cost;
     }
 
+    /**
+     * Instantiates a new Node.
+     *
+     * @param data the data
+     */
     public Node(int data) {
         this.data = data;
     }
 
+    /**
+     * Gets data.
+     *
+     * @return the data
+     */
     public int getData() {
         return data;
     }
@@ -54,18 +77,38 @@ public class Node implements Comparable<Node>, Comparator<Node> {
         return this.cost - o.cost;
     }
 
+    /**
+     * Sets dijkstra results map.
+     *
+     * @param dijkstraResultsMap the dijkstra results map
+     */
     public void setDijkstraResultsMap(HashMap<Node, Integer> dijkstraResultsMap) {
         this.dijkstraResultsMap = dijkstraResultsMap;
     }
 
+    /**
+     * Gets dijkstra results map.
+     *
+     * @return the dijkstra results map
+     */
     public HashMap<Node, Integer> getDijkstraResultsMap() {
         return dijkstraResultsMap;
     }
 
+    /**
+     * Sets fair score.
+     *
+     * @param fairScore the fair score
+     */
     public void setFairScore(float fairScore) {
         this.fairScore = fairScore;
     }
 
+    /**
+     * Gets fair score.
+     *
+     * @return the fair score
+     */
     public float getFairScore() {
         return fairScore;
     }
